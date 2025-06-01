e0ea9a6c4ffbccb6e663c9476fe655b2bc5e2184
Ripon-Mardy <mardyripon10@gmail.com>
2025-06-01 23:38:18 +0600

    fix github actions

--- frontend/next.config.mjs
+++ frontend/next.config.mjs
@@ -1,7 +1,22 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     images : {
-        domains : ['jonosokti-backend.s3.ap-south-1.amazonaws.com', '147.79.70.5']
+        domains : ['jonosokti-backend.s3.ap-south-1.amazonaws.com', '147.79.70.5'],
+        remotePatterns : [
+            {
+                protocol : 'https',
+                hostname : 'jonosokti-backend.s3.ap-south-1.amazonaws.com',
+                port : '',
+                pathname : '/**',
+            },
+             {
+                protocol : 'https',
+                hostname : '147.79.70.5',
+                port : '',
+                pathname : '/**',
+            },
+
+        ]
     },
     env : {
         NEXT_PUBLIC_JONOSOKTI_API_URL : process.env.NEXT_PUBLIC_JONOSOKTI_API_URL   
